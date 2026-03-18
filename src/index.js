87174f53ede072d336350a1b6f16c6813ae9bb58
rebecca967 <150416948+rebecca967@users.noreply.github.com>
2026-03-17 20:09:59 -0600

    added css and fix the index

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
-function displayIdeas (response){
-   new Typewriter('.food', {
-  strings: response.data.answer,
-  autoStart: true,
-  delay:2, 
-  cursor:""
-});
+function displayIdeas(response) {
 
+  let foodElement = document.querySelector(".food");
+
+  // Remove ```html if AI adds it
+  let recipe = response.data.answer
+    .replace(/```html/g, "")
+    .replace(/```/g, "");
+
+  foodElement.innerHTML = "";
+
+  new Typewriter(".food", {
+    strings: recipe,
+    autoStart: true,
+    delay: 2,
+    cursor: ""
+  });
 }
 
-
-
-function generateDesserts(event){
+function generateDesserts(event) {
   event.preventDefault();
 
-let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey= "4dab37b88309477t52afoc35f8fb2163";
-  let prompt =  `user instuction:Generate a short dessert recipe that ${instructionsInput.value} has submited`;
-  let context= "You are a dessert expert and love listing different recipes of desserts from around the world. Youre mission is  suggest different kinds of desserts that relates with user Instruction. Make sure to follow the user intruction ";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
- 
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "4dab37b88309477t52afoc35f8fb2163";
+
+  let prompt = `User ingredient: ${instructionsInput.value}. Generate 3 dessert recipes using this ingredient.`;
+
+  let context = "You are a dessert expert. Suggest 3 desserts related to the user's instruction. Return the answer in HTML. Use <strong> for the title and <br> for spacing. Under each dessert include a short description and a clickable link using <a href='https://www.google.com/search?q=DESSERT+RECIPE' target='_blank'>View Recipes</a>. Do not include markdown or code blocks.";
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let foodElement = document.querySelector(".food");
-foodElement.innerHTML = "⏳ Generating your dessert recipe...";
-  
-axios.get(apiURL).then(displayIdeas)
+  foodElement.innerHTML = "🍰 Generating  dessert ideas...";
 
-
- 
+  axios.get(apiURL).then(displayIdeas);
 }
 
 let dessertElement = document.querySelector("#dessert-idea-generator");
-dessertElement.addEventListener("submit", generateDesserts)
+dessertElement.addEventListener("submit", generateDesserts);
